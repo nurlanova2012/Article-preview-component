f250bb23d67bd78e3570741bb3ce546db6c30731
AN <83594157+nurlanova2012@users.noreply.github.com>
2024-04-16 12:48:09 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var shareIcon = document.getElementById("share-icon");
 var active = document.getElementById("active");
+var profile = document.getElementById("profile");
 
-
-shareIcon.addEventListener("click", function(e){
-  if (active.style.display === "none") {
+shareIcon.addEventListener("click", function (e) {
+  if (active.style.display === "none" || active.style.display === "") {
     active.style.display = "flex";
   } else {
     active.style.display = "none";
   }
-})
+});
+
+var x = window.matchMedia("(max-width:410px)");
+
+function handleViewportChange(x) {
+  if (x.matches) {
+    shareIcon.classList.add("social-mobile");
+    active.classList.add("active-mobile");
+  } else {
+    shareIcon.classList.remove("social-mobile");
+    active.classList.remove("active-mobile");
+  }
+}
+handleViewportChange(x);
+x.addEventListener(handleViewportChange);
